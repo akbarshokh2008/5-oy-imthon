f7e45301ec5aaa23ae96cc014565b5798968c8f2
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-04-26 21:42:49 +0500

    j

--- js/details.js
+++ js/details.js
@@ -24,55 +24,64 @@ darkBtn.addEventListener("click", () => {
   }
 });
 
+const loader = document.querySelector("#loader");
+
 const urlParams = new URLSearchParams(window.location.search);
-const region = urlParams.get("region");
-const id = urlParams.get("id");
+const slug = urlParams.get("slug");
 getdata();
 
 function getdata() {
-  fetch(
-    `https://frontend-mentor-apis-6efy.onrender.com/countries?region=${region}&search=${id}`
-  )
+  fetch("https://frontend-mentor-apis-6efy.onrender.com/countries/" + slug)
     .then((res) => res.json())
     .then((data) => {
-      createDav(data);
+      createEl(data);
+      loader.classList.add("hidden");
     });
 }
 
-function createDav(todoArr) {
-  const hammasi = document.querySelector("#hammasi"); // 1. List elementini tanlash
-  hammasi.innerHTML = ""; // 2. Listning ichini tozalash
-  todoArr.data.forEach((item) => {
-    const article = document.createElement("article");
-    article.classList.add("list-item");
-    article.innerHTML += `
-      <section class="container main-container">
-        <a class="about-href" href="index.html">
-          <img src="./img/back.svg" alt="strelka" />Back
-        </a>
-        <div class="main-dav">
-          <img src=${item.flags.png} alt=${item.flags.alt} />
-          <div class="main-text">
-            <h2>${item.name.common}</h2>
-            <div class="main-text__1">
-              <p><b>Native Name: </b>${item.name.nativeName}</p>
-              <p><b>Population: </b>${item.population}</p>
-              <p><b>Region: </b>${item.region}</p>
-              <p><b>Sub Region: </b>${item.subregion}</p>
-              <p><b>Capital: </b>${item.capital}</p>
-            </div>
-            <div class="main-text__2">
-              <p><b>Top Level Domain: </b>${item.continents}</p>
-              <p><b>Currencies: </b>${item.currencies}</p>
-              <p><b>Languages: </b>${item.languages}</p>
-            </div>
-            <div class="main-text-border">
-              <b>Border Countries: </b><a href="">${item.borders}</a>
-            </div>
-          </div>
+const hammasi = document.querySelector("#hammasi");
+
+function createEl(todoDav) {
+  hammasi.innerHTML = "";
+  const borders = todoDav.borders
+    .map(
+      (borders) =>
+        `<a href="about.html?slug=${todoDav.borders.slug}">${borders.slug}</a>`
+    )
+    .join(", ");
+  const section = document.createElement("section");
+  section.classList.add("list-item-dav");
+  section.innerHTML += `
+  <section class="container main-container">
+  <a class="about-href" href="index.html">
+    <img src="./img/back.svg" alt="strelka" />
+    Back
+  </a>
+  <div class="main-dav">
+    <img src="${todoDav.flags.png}" alt="${todoDav.flags.alt}" />
+    <div class="main-text">
+      <h2>${todoDav.name.common}</h2>
+      <div class="main-textlar">
+        <div class="main-text__1">
+          <p><b>Native Name: </b>${todoDav.name.nativeName}</p>
+          <p><b>Population: </b>${todoDav.population}</p>
+          <p><b>Region: </b>${todoDav.region}</p>
+          <p><b>Sub Region: </b>${todoDav.subregion}</p>
+          <p><b>Capital: </b>${todoDav.capital}</p>
         </div>
-      </section>
+        <div class="main-text__2">
+          <p><b>Top Level Domain: </b>${todoDav.continents}</p>
+          <p><b>Currencies: </b>${todoDav.currencies}</p>
+          <p><b>Languages: </b>${todoDav.languages}</p>
+        </div>
+      </div>
+      <div class="main-text-border">
+        <b>Border Countries: </b><a href="">${borders}</a>
+      </div>
+    </div>
+  </div>
+</section> 
+ 
     `;
-    hammasi.appendChild(article);
-  });
+  hammasi.appendChild(section);
 }
